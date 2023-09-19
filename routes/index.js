45e6b02d90b6039db93a5d2d485d0be8307a1125
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-09-19 09:26:48 +0300

    Add event routes

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const projectsRouter = require('./projects')
 app.use('/projects', projectsRouter)
 
 const eventsRouter = require('./events')
-app.use('events', eventsRouter);
+app.use('/events', eventsRouter);
 
 const { any } = require('webidl-conversions');
 app.use(express.urlencoded({extended: true}))
