d1e02bf9dd9bb5c1218a798fac2d9d3020d4d1f1
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-09-21 17:42:21 +0300

    Add more endpoints to member

--- routes/members.js
+++ routes/members.js
@@ -39,15 +39,19 @@ router.get(
 );
 
 router.delete(
-  "/:id",
+  "deleteMember/:id",
   asyncHandler(async (req, res) => {
     const memberId = req.params.id;
-    const member = await Member.findById(memberId);
+    const member = await Member.findById(memberId);const { any } = require('webidl-conversions');
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
 
     if (!member) {
       return res.status(404).json({ error: "Member not found" });
     }
-
+    const { any } = require('webidl-conversions');
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
     await Member.deleteOne({ _id: memberId })
       .then((deletedMember) => {
         res.status(200).send(deletedMember);
@@ -57,5 +61,64 @@ router.delete(
       });
   })
 );
+
+//Get a paginated list of members (3 per page)
+router.get("/getAll/:pageNumber", async(req,res)=>{
+    const pageNumber = req.params.pageNumber || 1;
+    const pageSize = 3;
+    try{
+        const allMembers = await members.find({}).sort({registered_on: -1}).skip((pageNumber - 1)*pageSize).limit(pageSize)
+        res.send(allMembers)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+//get member by Id
+router.get("/getMemberById/:memberId", async(req, res)=>{
+    try{
+        const theMember = await members.findById(req.params.memberId)
+        if(!theMember){
+            res.status(404).send("No such member");
+            return;
+        }
+        res.send(theMember);
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+})
+
+//Edit a member's profile
+router.put("/editProfile/:memberId", async(req, res)=>{
+    try{
+        const memberId = req.params.memberId;
+        const editedProfile = req.body;
+
+        const theNewProfile = await members.findByIdAndUpdate(memberId, editedProfile);
+        res.send(theNewProfile);
+
+    }catch(e){
+        res.status(500).send(e);
+    }
+})
+
+//Search for member by name
+router.get("/searchByName", async(req, res)=>{
+    const queryString = req.query.queryString;
+    const regex = new RegExp(queryString, 'i');
+    try{
+        const searchResult = await members.find({
+            $or: [
+                {first_name: {$regex: regex}},
+                {last_name: {$regex: regex}}
+            ]
+        })
+        res.send(searchResult);
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+})
 
 module.exports = router;
