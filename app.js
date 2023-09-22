48d713ae28a2e6ee9814822a990e99d73af79e85
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-09-22 10:42:22 +0300

    Move app.js to root dir

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const { any } = require("webidl-conversions");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //the middlewares should be declared before the matters since the order matters
+
+const db = require("./connections");
+
+const trainingsRouter = require("./routes/trainings");
+app.use("/trainings", trainingsRouter);
+
+const membersRouter = require("./routes/members");
+app.use("/members", membersRouter);
+
+const newsRouter = require("./routes/news");
+app.use("/news", newsRouter);
+
+const projectsRouter = require("./routes/projects");
+app.use("/projects", projectsRouter);
+
+const eventsRouter = require('./routes/events')
+app.use('/events', eventsRouter);
+
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080")
+})
+
+
