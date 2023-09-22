02ea4c9f34b2702cd50add36de5887832fbf5dd7
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-09-22 10:42:43 +0300

    Modified news route

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,24 @@
-const news = require('../models/news');
 const express = require('express');
 const router = express.Router();
+const { getAllNews, createNews, getNews, getNewsBylimit, updateNews, deleteNews } 
+        = require('../controllers/news.js')
+const uploadImage = require('../config/multer.js')
 
-//endpoints go here
+
+//handle route for query parameters prior to fetching all news
+router.get('/query', getNewsBylimit,getAllNews )
+
+//GET
+router.get('/', getAllNews)
+router.get('/:id', getNews)
+//POST -admin only
+router.post('/', uploadImage.single("image"), createNews)
+//PUT [update]
+router.put('/:id', uploadImage.single("image"), updateNews)
+//DELETE
+router.delete('/:id', deleteNews)
+
+
+
 
 module.exports = router;
