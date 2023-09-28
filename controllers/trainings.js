d104e159905a6a6c94c09a93f8bf7e0023994e39
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-09-28 05:22:19 +0300

    Add controller for training route

---
+++ controllers/trainings.js
@@ -0,0 +1,117 @@
+const training = require('../models/trainings')
+
+const createTraining = async(req,res)=>{
+    try{
+        // console.log(req.body)
+        // const {title, description, starts_on, ends_on} = req.body;
+        // training.create({title, description, starts_on, ends_on}).then((train) =>{console.log(train)}).catch((error)=>{console.log(error)})
+        const newTraining = new training({
+            title: req.body.title,
+            description: req.body.description,
+            starts_on: req.body.starts_on,
+            ends_on: req.body.ends_on,
+            status: req.body.status
+        })
+        await newTraining.save()
+        res.send(newTraining)
+    }
+    catch(error){
+        // const newTraining  = new training({
+        //     title : req.body.title,
+        //     description : req.body.description,
+        //     starts_on : req.body.starts_on,
+        //     ends_on : req.body.ends_on
+        // })
+        // await newTraining.save()
+        let request = req;
+        // console.log(req);
+        res.send({"one": `${req.body.body}`})
+    }
+    
+}
+
+
+const getAllTrainings = async(req, res)=>{
+    try{
+        const trainings = await training.find({}).sort({starts_on : -1})
+        res.send(trainings);
+    }catch(error){
+        res.status(500).send(error)
+    }    
+};
+
+const findById = async(req,res)=>{
+    try{
+        console.log(req.params.id)
+        const oneTraining = await training.findOne({
+            _id: req.params.id
+        })
+        res.send(oneTraining)
+    }catch(error){
+        res.status(500).send(error)
+    }
+};
+
+const updateTraining = async(req,res)=>{
+    id = req.params.id;
+    const exists = await training.findOne({_id: id})
+    if(!exists){
+        res.status(404).send("Training not found") 
+        return;
+    }
+    const updated = {$set:{
+        title:req.body.title,
+        description:req.body.description,
+        starts_on:req.body.starts_on,
+        status:req.body.status
+    }}
+    const filter = {_id: id}
+    try{
+        await training.updateOne(filter, updated);
+        res.send(await training.findOne({_id: id}))
+    }catch(error){
+        res.status(500).send(error)
+    }
+}
+
+const getPaginated = async(req, res)=>{
+    const pageNumber = req.params.pageNumber || 1;
+    const pageSize = 3;
+    try{
+        const paginatedTrainings = await training.find({}).sort({starts_on : 1}).skip((pageNumber - 1)*pageSize).limit(pageSize)
+        res.json(paginatedTrainings)
+    }
+    catch(error){
+        res.status(500).send(error);
+    }  
+}
+
+const getByStatus = async(req, res)=>{
+    const key = req.query.key;
+    if( !(key in ["Done", "Underway", "Upcoming"])){
+        res.json({"error": "No such status"})
+    }
+    try{
+        const trainings = await training.find({status: key})
+        res.send(trainings)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
+
+const searchTraining = async(req, res)=>{
+    const key = req.query.key;
+    const regex = new RegExp(key, i);
+    try{
+        const trainings = await projects.find({
+            $or:[{title: {$regex: regex}},
+                 {description: {$regex: regex}}
+                ]
+        })
+        res.send(trainings)
+    }catch(e){
+        res.send(e)
+    }
+}
+
+module.exports = {createTraining, getAllTrainings, findById, updateTraining, getPaginated, getByStatus, searchTraining}
--- routes/trainings.js
+++ routes/trainings.js
@@ -2,128 +2,32 @@ const training = require('../models/trainings')
 const express = require('express');
 const router = express.Router();
 
-router.use(express.urlencoded({extended: true}))
-router.use(express.json())
+const controllers = require('../controllers/trainings');
+const projects = require('../models/projects');
+
 
 //Add new training to the database
-router.post("/create", async(req,res)=>{
-    try{
-        // console.log(req.body)
-        // const {title, description, starts_on, ends_on} = req.body;
-        // training.create({title, description, starts_on, ends_on}).then((train) =>{console.log(train)}).catch((error)=>{console.log(error)})
-        const newTraining = new training({
-            title: req.body.title,
-            description: req.body.description,
-            starts_on: req.body.starts_on,
-            ends_on: req.body.ends_on,
-            status: req.body.status
-        })
-        await newTraining.save()
-        res.send(newTraining)
-    }
-    catch(error){
-        // const newTraining  = new training({
-        //     title : req.body.title,
-        //     description : req.body.description,
-        //     starts_on : req.body.starts_on,
-        //     ends_on : req.body.ends_on
-        // })
-        // await newTraining.save()
-        let request = req;
-        // console.log(req);
-        res.send({"one": `${req.body.body}`})
-    }
-    
-})
+router.post("/create",controllers.createTraining )
 
 
 //List all the trainings in the database
-router.get("/", async(req, res)=>{
-    try{
-        const trainings = await training.find({}).sort({starts_on : -1})
-        res.send(trainings);
-    }catch(error){
-        res.status(500).send(error)
-    }    
-})
+router.get("/", controllers.getAllTrainings)
 
 //Get a training by id
-router.get("/:id", async(req,res)=>{
-    try{
-        console.log(req.params.id)
-        const oneTraining = await training.findOne({
-            _id: req.params.id
-        })
-        res.send(oneTraining)
-    }catch(error){
-        res.status(500).send(error)
-    }
-})
+router.get("findById/:id", controllers.findById)
 
 //update a training specified by id
-router.put("/:id", async(req,res)=>{
-    id = req.params.id;
-    const exists = await training.findOne({_id: id})
-    if(!exists){
-        res.status(404).send("Training not found") 
-        return;
-    }
-    const updated = {$set:{
-        title:req.body.title,
-        description:req.body.description,
-        starts_on:req.body.starts_on,
-        status:req.body.status
-    }}
-    const filter = {_id: id}
-    try{
-        await training.updateOne(filter, updated);
-        res.send(await training.findOne({_id: id}))
-    }catch(error){
-        res.status(500).send(error)
-    }
-})
+router.put("update/:id", controllers.updateTraining)
 
 //Paginated view of trainings (3 at a time) sorted by start time 
-router.get("/paginated/:pageNumber", async(req, res)=>{
-    const pageNumber = req.params.pageNumber || 1;
-    const pageSize = 3;
-    try{
-        const paginatedTrainings = await training.find({}).sort({starts_on : 1}).skip((pageNumber - 1)*pageSize).limit(pageSize)
-        res.json(paginatedTrainings)
-    }
-    catch(error){
-        res.status(500).send(error);
-    }  
-})
+router.get("/paginated/:pageNumber", controllers.getPaginated)
 
-//Get all trainings that are of "Done" status
+//Get trainings by status
+router.get("/status/", controllers.getByStatus)
 
-router.get("/status/done", async(req, res)=>{
-    try{
-        const done = await training.find({status: "Done"})
-        res.send(done)
-    }catch(error){
-        res.status(500).send(error.message)
-    }
-})
+//Search for a training
 
-router.get("/status/underway", async(req, res)=>{
-    try{
-        const underway = await training.find({status: "Underway"})
-        res.send(underway)
-    }catch(error){
-        res.status(500).send(error)
-    }
-})
-
-router.get("/status/upcoming", async(req, res)=>{
-    try{
-        const  upcoming = await training.find({status: "Upcoming"})
-        res.send(upcoming)
-    }catch(error){
-        res.status(500).send(error)
-    }
-})
+router.get("/search", controllers.searchTraining)
 
 
 
